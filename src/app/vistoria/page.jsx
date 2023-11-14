56f4fd3d25ce6bcd8b350f00404225e4a083c923
nichol6s <nicholas.jake15@gmail.com>
2023-11-13 21:23:30 -0300

    QrCode implementado na página de vistoria

--- src/app/vistoria/page.jsx
+++ src/app/vistoria/page.jsx
@@ -2,13 +2,14 @@ import Layout from "@/components/Layout";
 import Link from "next/link";
 
 export default function Vistoria(){
+    const qrCodeValue = 'https://challenge-web-next.vercel.app/vistoria-mobile';
     return(
         <Layout>
             <main>
                 <img src="/img-porto-logo.png" alt="Porto Seguro's logo with background"/>
                 <form className="container">
                     <h1 className="container__title">Escaneie o QR Code para iniciar a vistoria</h1>
-                    <p>QR Code Aqui</p>
+                    <img src={`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qrCodeValue)}`} alt="QR Code" />
                     <p>O usuário será redirecionado para a página mobile da vistoria ao escanear</p>      
                     <Link className="link-to campos__buttons__layout" href="/vistoria-mobile">Continuar</Link>
                 </form>
