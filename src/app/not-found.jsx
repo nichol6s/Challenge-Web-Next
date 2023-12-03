66e07aff5f690b494f050116e70db9684eb1fac0
k-alm <kauan.alm.almeida@gmail.com>
2023-12-03 20:27:47 -0300

    bug do css do erro404 corrigido

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
 import Link from "next/link";
-import "../css/error404.css"
+import "../css/error404.css";
 
-export default function Error404() {
-    //document.body.classList.add('stop-animation');
+export default function Error404() {   
     return (
         <main className="error-main">
             <section className="error-section">
-                <Image src="/bike-erro.png" alt="Porto Seguro's logo with background" width={225} height={225} />
+                <img className="image" src="/bike-erro.png" alt="Porto Seguro's logo with background" width={225} height={225} />
                 <div className="error-container">
                     <h1 className="error-title principal">404...</h1>
                     <h2 className="error-title secundario">Caminho errado!</h2>
